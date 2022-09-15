3df6e8400613548192b9cd2c99ac1be0167607bf
Unkn0wn-7 <GM21007@ues.edu.sv>
2022-09-15 01:09:46 -0600

    Creacion del crud
    Se ha implementado el poder ingresar productos, se desarrollo la tabla de datos y el formulario de ingreso de datos

---
+++ CRUDProducto/database/migrations/2022_09_15_055805_create_productos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id();
+            $table->String('Codigo');
+            $table->String('Descripcion');
+            $table->integer('Cantidad');
+            $table->decimal('Presio',8,2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productos');
+    }
+}
---
+++ CRUDProducto/resources/views/producto/index.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.plantillabase');
+
+@section('contenido')
+<a href="productos/create" class = "btn btn-primary">CREAR</a>
+<table class = "table table-dark table-striped mt-4">
+    <thead>
+        <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Codigo</th>
+            <th scope="col">Descripcion</th>
+            <th scope="col">Cantidad</th>
+            <th scope="col">Precio</th>
+            <th scope="col">Acciones</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($productos as $pro)
+        <tr>
+            <td>{{$pro->id}}</td>
+            <td>{{$pro->Codigo}}</td>
+            <td>{{$pro->Descripcion}}</td>
+            <td>{{$pro->Cantidad}}</td>
+            <td>{{$pro->Presio}}</td>
+            <td>
+                <a class = "btn btn-info">Editar</a>
+                <button class="btn btn-danger">Borrar</button>
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
+@endsection
